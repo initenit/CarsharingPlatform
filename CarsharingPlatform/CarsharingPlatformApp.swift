983ad0dc71f874f23ad2576add91d73100eb63b0
initenit <ini_tenit@naver.com>
2021-12-17 00:10:44 +0900

    Initial Commit

---
+++ CarsharingPlatform/CarsharingPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsharingPlatformApp.swift
+//  CarsharingPlatform
+//
+//  Created by Euijae Lee on 2021/12/17.
+//
+
+import SwiftUI
+
+@main
+struct CarsharingPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarsharingPlatform/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarsharingPlatform
+//
+//  Created by Euijae Lee on 2021/12/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
